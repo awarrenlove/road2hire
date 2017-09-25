74793c1eaae31c288ea2cfbbabe4b385189ccaa6
Andrew Warren-Love <alove@redventures.com>
2017-09-25 15:12:54 -0400

    Add console application with a Timer for multiple bell calls

---
+++ bell-timer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+using System.Threading;
+
+namespace bell_timer
+{
+    class Program
+    {
+        static int Interval { get; set; } = 1000;
+
+        static int Length { get; set; } = 3;
+
+        static void Tick(object source, ElapsedEventArgs e)
+        {
+            for (var i = 0;i < Length;i++)
+            {
+                Console.Write("\a\b");
+                Thread.Sleep(150);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+
+            timer.Elapsed += Tick;
+            timer.Interval = Interval;
+            timer.Enabled = true;
+
+            Console.ReadKey();
+        }
+    }
+}
